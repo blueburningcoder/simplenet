5f45fa367e1652ff93cfa3b472bd2093e96e9cac
blueburningcoder <f.karg10@gmail.com>
2017-09-13 14:22:16 +0200

    sync

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 extern crate simplenetlib;
 
-use simplenetlib::test;
+use simplenetlib::*;
 
 
 fn main() {
     test();
     print!("Hello World!");
+    /*
+    let trainL = read_labels("train-labels-idx1-ubyte.gz")
+                    .expect("failed to read file");
+    let trainN = read_numbers("train-images-idx3-ubyte.gz")
+                    .expect("failed to read file");
+    */
+
+
+    // let testL = read_labels("t10k-labels-idx1-ubyte.gz")
+    //                 .expect("failed to read file");
+    let testN = read_numbers("t10k-images-idx3-ubyte.gz")
+                    .expect("failed to read file");
 }
